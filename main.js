ee172bf44b712425efd84fd8dce6376c1d007304
luyckxrobbe <48286055+luyckxrobbe@users.noreply.github.com>
2023-03-29 11:29:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tds = document.querySelectorAll("td");
 
-  // Load the previously selected TDs from the cookies
-  const selectedRuns = JSON.parse(getCookie("selectedRuns")) || [];
-  selectedRuns.forEach((run) => {
-    const cell = document.querySelector(
-      `td[data-week="${run.week}"][data-run="${run.run}"]`
-    );
-    cell.classList.add("done");
-  });
-
+  // Load stored IDs when the page is loaded
+  const storedIds = JSON.parse(localStorage.getItem("selected")) || [];
+  for (const storedId of storedIds)
+    document.getElementById(storedId).classList.add("done");
   tds.forEach((td) => {
     td.addEventListener("click", function () {
-      const week = this.dataset.week;
-      const run = this.dataset.run;
-      const clickedId = { week, run };
-      const selectedRuns = JSON.parse(getCookie("selectedRuns")) || [];
-      const index = selectedRuns.findIndex(
-        (r) => r.week === clickedId.week && r.run === clickedId.run
-      );
+      const currentContent = this.innerHTML;
+      // Check if td has the 'done' class
+      const hasDoneClass = this.classList.contains("done");
 
-      if (index > -1) {
-        selectedRuns.splice(index, 1);
-      } else {
-        selectedRuns.push(clickedId);
+      // Check if the clicked td's id is already stored
+      const clickedId = this.id;
+      const index = storedIds.indexOf(clickedId);
+
+      // Add or remove the clicked td's id from the storedIds array
+      if (!hasDoneClass && index === -1) {
+        storedIds.push(clickedId);
+      } else if (hasDoneClass && index > -1) {
+        storedIds.splice(index, 1);
       }
 
-      setCookie("selectedRuns", JSON.stringify(selectedRuns));
+      // Store the updated storedIds array in localStorage
+      localStorage.setItem("selected", JSON.stringify(storedIds));
 
       // Toggle the 'done' class on the clicked TD
       this.classList.toggle("done");
     });
   });
-
-  // Helper functions to set and get cookies
-  function setCookie(name, value, days = 7) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${encodeURIComponent(
-      value
-    )}; expires=${expires}; path=/`;
-  }
-
-  function getCookie(name) {
-    return document.cookie.split("; ").reduce((r, v) => {
-      const parts = v.split("=");
-      return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-    }, "");
-  }
 });
